9851de237d8e6d69551df4374039e622e4fbafbc
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-09 20:51:11 +0100

    Update

--- src/EvemonkApp.jsx
+++ src/EvemonkApp.jsx
@@ -1,9 +1,14 @@
+import TopAlliances from "./TopAlliances.jsx";
+import TopCorporations from "./TopCorporations.jsx";
+
 import './EvemonkApp.css'
 
 function EvemonkApp() {
   return (
     <>
       <h1>Aloha!</h1>
+      <TopAlliances />
+      <TopCorporations />
     </>
   )
 }
