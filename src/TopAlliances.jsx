bdee3959ba320d54e2c440b8fe4ad2eea15227b4
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-09 22:20:15 +0100

    Update

--- src/TopAlliances.jsx
+++ src/TopAlliances.jsx
@@ -4,11 +4,13 @@ import './TopAlliances.css';
 
 const GET_TOP_ALLIANCES = gql`
   query getAlliances {
-    alliances(first: 2) {
+    alliances(first: 15) {
       edges {
         node {
           id
           name
+          corporationsCount
+          charactersCount
         }
       }
     }
@@ -26,15 +28,32 @@ function TopAlliances() {
   return (
     <>
       <div className={"col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6"}>
-
-
-        <h1>Top alliances</h1>
+        <h4 className="text-center">Top alliances</h4>
 
         <table className={"table table-dark table-striped table-bordered"}>
+          <thead>
+            <tr>
+              <th colSpan="2" className="text-center">Name</th>
+              <th className="text-end">Corps</th>
+              <th className="text-end">Members</th>
+            </tr>
+          </thead>
+
           <tbody>
             {data.alliances.edges.map((edge) => (
               <tr key={edge.node.id}>
-                <td><a href={edge.node.id}>{edge.node.name}</a></td>
+                <td className="text-center align-middle border-end-0 m-0 p-2">
+                  &nbsp;
+                </td>
+                <td className="text-break align-middle border-start-0">
+                  <a href={edge.node.id}>{edge.node.name}</a>
+                </td>
+                <td className="text-end align-middle">
+                  {edge.node.corporationsCount}
+                </td>
+                <td className="text-end align-middle">
+                  {edge.node.charactersCount}
+                </td>
               </tr>
             ))}
           </tbody>
