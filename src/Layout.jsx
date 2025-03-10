87ae72a17d772807f075ee01758a3e5e8160b3e2
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-10 01:03:08 +0100

    Update

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,9 +1,13 @@
 import './Layout.css'
+
+import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
 
 function Layout({ children }) {
   return (
     <>
+      <Header/>
+
       <div className={"container"}>
         {children}
       </div>
