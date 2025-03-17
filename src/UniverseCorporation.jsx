747de0d95d3ec66fc24b709a8ca0b72018f43b83
Igor Zubkov <igor.zubkov@gmail.com>
2025-03-17 22:43:10 +0100

    Update

--- src/UniverseCorporation.jsx
+++ src/UniverseCorporation.jsx
@@ -1,15 +1,41 @@
 import { useParams } from "react-router";
 
+import { gql, useQuery } from "@apollo/client";
+
 import Layout from "./Layout";
+
+const GET_CORPORATION_BY_ID = gql`
+  query getCorporationByID($id: ID!) {
+    corporation(id: $id) {
+      id
+      name
+    }
+  }
+`;
 
 function UniverseCorporation() {
   let { id } = useParams();
 
   console.log(id);
 
+  const { loading, error, data } = useQuery(GET_CORPORATION_BY_ID, {
+    variables: { id },
+  });
+
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+
+  console.log(data);
+
   return (
     <>
-      <Layout></Layout>
+      <Layout>
+        <div className="row">
+          <div className="col-12 col-sm-12 col-md-12 col-lg-4 col-xl-4">
+            <h4 className="text-center text-break">Corporation: {data.corporation.name}</h4>
+          </div>
+        </div>
+      </Layout>
     </>
   )
 }
